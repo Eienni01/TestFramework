963a5a3b82264dd73272ec4a86e8177e239bc83d
Eienni01 <subashgauchan12@gmail.com>
2024-06-03 20:36:54 +1000

    push

--- src/test/java/Testrunner/TestRunner.java
+++ src/test/java/Testrunner/TestRunner.java
@@ -11,7 +11,7 @@ import io.cucumber.junit.CucumberOptions;
 		features= "src/test/java/Features/login.feature",
 		glue = {"Stepdefinition"},
 		plugin= {
-				"pretty", "html:target/report/cucumber.html", "json:target/cucumber-reports"
+				"pretty", "html:target/report/cucumber.html", "json:target/cucumber.json"
 		}
 )
 public class TestRunner{}
